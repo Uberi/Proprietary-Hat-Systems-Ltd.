8f2d7958ba8d84f4ac45286dca3f9f7a1be1440f
Lance Bantoto <lancewbantoto@gmail.com>
2015-01-31 05:42:38 -0500

    removed unicode

--- script.py
+++ script.py
@@ -1,19 +1,22 @@
-from googlemaps import googlemaps
+from googlemaps 
+import googlemaps
 import apikey
 import json
 
 gmaps = googlemaps.Client(key=apikey.key())
 
-#departure = "40 Saint George Street Toronto, ON M5S 2E4"
-#destination = "349 College Street Toronto, ON M5T 1S5 Canada"
+"""
+departure = "40 Saint George Street Toronto, ON M5S 2E4 Canada"
+destination = "349 College Street Toronto, ON M5T 1S5 Canada"
+"""
 
 departure = "Sydney"
 destination = "Melbourne"
 
-routes = gmaps.directions(departure, destination, mode="walking")
+routes = gmaps.directions(departure, destination, mode = "walking")
 
-steps = routes[0][u'legs'][0][u'steps']
+steps = routes[0]['legs'][0]['steps']
 
-waypoints = map(lambda waypoint: waypoint[u'start_location'], steps)
-waypoints.append(steps[-1][u'end_location'])
+waypoints = map(lambda waypoint: waypoint['start_location'], steps)
+waypoints.append(steps[-1]['end_location'])
 print waypoints
